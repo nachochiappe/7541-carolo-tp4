afe1e969a1f4c8915bcc282ba9a9dfde59d9c9e7
Ignacio Chiappe <nachochiappe@users.noreply.github.com>
2017-11-15 12:45:21 -0300

    Modificaciones varias.

--- main.c
+++ main.c
@@ -16,30 +16,30 @@ int main (int argc, char *argv[]) {
 
 	char inst[3];
 	char palabra[20];
-	TLista *lResultado;
+	TLista lResultado;
 
 	TDAGlosario *g = (TDAGlosario*) malloc(sizeof(TDAGlosario));
 	if (!g) return (-1);
 
 	ls_Crear(&lResultado, sizeof(TPalabra));
 
-	if (!CrearGlosario(&g, argv[1], argv[2])) return (1);
+	if (!CrearGlosario(g, argv[1], argv[2])) return (1);
 
 	FILE *arch_instrucciones = fopen(argv[3],"r");
 
 	while (fgets(inst, sizeof(inst), arch_instrucciones) != NULL) {
 		if (strcmp(inst, "cp ") == 0) {
 			fgets(palabra, sizeof(palabra), arch_instrucciones);
-			if (ConsultarpalabraGlosario(&g, palabra, &lResultado) != 0) return (1);
+			if (ConsultarpalabraGlosario(g, palabra, &lResultado) != 0) return (1);
 		}
 		else if (strcmp(inst, "rp") == 0) {
-			if (Ranking_palabras_Glosario(&g, &lResultado) != 0) return (1);
+			if (Ranking_palabras_Glosario(g, &lResultado) != 0) return (1);
 		}
 		else
 			printf("%s es una instrucción errónea.\n", inst);
 	}
 
-	if (!DestruirGlosario(&g)) return (1);
+	if (!DestruirGlosario(g)) return (1);
 
 	return (0);
 }
